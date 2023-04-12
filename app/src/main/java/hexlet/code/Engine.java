953a45dac76d9fad9873df80e9fb82324f065a44
djwinnn <djwinnn@mail.ru>
2023-04-12 21:28:14 +0300

    fix checkstyle

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -6,8 +6,8 @@ public class Engine {
     public static final int NUMBER_OF_ROUNDS = 3;
     public static final int NUMBERS_LIMIT = 100;
     public static final int LIMIT_OBJECT_IN_ARRAY = 3;
-    private static final String[] QUESTIONS = new String[LIMIT_OBJECT_IN_ARRAY];
-    private static final String[] ANSWERS_MACHINE = new String[LIMIT_OBJECT_IN_ARRAY];
+    public static String[] QUESTIONS = new String[LIMIT_OBJECT_IN_ARRAY];
+    public static String[] ANSWERS_MACHINE = new String[LIMIT_OBJECT_IN_ARRAY];
 
     public static String getName() {
         return name;
@@ -18,7 +18,7 @@ public class Engine {
     public static String[] getAnswersMachine() {
         return ANSWERS_MACHINE;
     }
-    public static void engine(String[] QUESTIONS, String[] ANSWERS_MACHINE, String taskGame) {
+    public static void engine(String[] questions, String[] answersMachine, String taskGame) {
 
         int count = 0;
         System.out.println(taskGame);
